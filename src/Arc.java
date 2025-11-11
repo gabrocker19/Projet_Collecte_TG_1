3c94cfe424b356e09501dc00b7bf0c4780d8895a
Aurelien <aurelien.kammerer@yahoo.com>
2025-11-11 14:41:45 +0100

    Ajout .txt

--- src/Arc.java
+++ src/Arc.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Arc {
     public Sommet s_depart, s_arrivee;
     public int distance;
@@ -5,5 +10,26 @@ public class Arc {
     public Arc (Sommet s1, Sommet s2){
         this.s_depart=s1;
         this.s_arrivee=s2;
+    }
+
+    public void remplir_tableau (ArrayList<Arc> Arcs) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\arcs_test.txt"))) {
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+                String[] parties = ligne.split(",");
+                int a = Integer.parseInt(parties[0].trim());
+                int b = Integer.parseInt(parties[1].trim());
+                Sommet s1=new Sommet(a,  0);
+                Sommet s2=new Sommet(b, 0);
+                Arc arc=new Arc(s1, s2);
+                Arcs.add(arc);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void afficher_sommet () {
+        System.out.println(this.s_depart.numero + " " + this.s_arrivee.numero);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
 import java.io.*;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<Arc> Arcs = new ArrayList<Arc>();
-        try (BufferedReader br = new BufferedReader(new FileReader("src\\arcs_test.txt"))) {
-            String ligne;
-            while ((ligne = br.readLine()) != null) {
-                String[] parties = ligne.split(",");
-                int a = Integer.parseInt(parties[0].trim());
-                int b = Integer.parseInt(parties[1].trim());
-                Sommet s1=new Sommet(a,  0);
-                Sommet s2=new Sommet(b, 0);
-                Arc arc=new Arc(s1, s2);
-                Arcs.add(arc);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        ArrayList<Arc> Arcs = new ArrayList<>();
+
+        Arc a = new Arc(null, null);
+        a.remplir_tableau(Arcs);
         for (Arc arc : Arcs) {
-            System.out.println(arc.s_depart.numero + " " + arc.s_arrivee.numero);
+            arc.afficher_sommet();
         }
     }
 }
