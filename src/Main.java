7f477c7178f667340a6b1a89d8b3c4aab3a17f7a
Brice Dupont <brice.dupont@gmail.com>
2025-11-19 17:14:25 +0100

    main modif avec tous les appels de Graphstream

--- src/Main.java
+++ src/Main.java
@@ -20,29 +20,39 @@ public class Main {
 
         Graphe graphe = new Graphe(m);
 
-        //T1_P1_H1 i1 = T1_P1_H1.creer(graphe);
-        //ResultatDijkstra r1 = graphe.Dijkstra(graphe, i1.s_depart.numero);
-        //graphe.afficher_distances(r1.distance);
-        //i1.remplir_itineraire(r1, graphe);
-        //i1.afficher_chemin();
+        /*T1_P1_H1 i1 = T1_P1_H1.creer(graphe);
+        ResultatDijkstra r1 = graphe.Dijkstra(graphe, i1.s_depart.numero);
+        graphe.afficher_distances(r1.distance);
+        i1.remplir_itineraire(r1, graphe);
+        i1.afficher_chemin();
+        Graphstream.creer_Graphstream(Arcs);
+        Graphstream.chemin_rouge(i1.chemin);*/
 
-        //T1_P1_H2 i2 = T1_P1_H2.creer(graphe);
-        //i2.afficher_sommets_a_visiter();
-        //i2.calculer_tsp();
-        //i2.afficher_matrice_tsp();
-        //i2.afficher_chemin();
+        /*T1_P1_H2 i2 = T1_P1_H2.creer(graphe);
+        i2.afficher_sommets_a_visiter();
+        i2.calculer_tsp();
+        i2.afficher_matrice_tsp();
+        i2.afficher_chemin();
+        Graphstream.creer_Graphstream(Arcs);
+        Graphstream.chemin_rouge(i2.meilleur_cycle);*/
 
-        //T1_P2_H1 i3 = new T1_P2_H1(graphe);
-        //i3.eulerianCycle();
-        //i3.afficher_chemin();
+        /*T1_P2_H1 i3 = new T1_P2_H1(graphe);
+        i3.eulerianCycle();
+        i3.afficher_chemin();
+        Graphstream.creer_Graphstream(Arcs);
+        Graphstream.chemin_rouge(i3.sommets);*/
 
-        //T1_P2_H2 i4 = new T1_P2_H2(graphe);
-        //i4.eulerPrime();
-        //i4.afficher_chemin();
+        /*T1_P2_H2 i4 = new T1_P2_H2(graphe);
+        i4.eulerPrime();
+        i4.afficher_chemin();
+        Graphstream.creer_Graphstream(Arcs);
+        Graphstream.chemin_rouge(i4.sommets);*/
 
-        T1_P2_H3 i5 = new T1_P2_H3(graphe);
+        /*T1_P2_H3 i5 = new T1_P2_H3(graphe);
         i5.chinesePostman();
         i5.afficher_chemin();
+        Graphstream.creer_Graphstream(Arcs);
+        Graphstream.chemin_rouge(i5.sommets);*/
 
         //Affichage aff = new Affichage(Arcs, m, graphe);
         //aff.lancerGUI();
